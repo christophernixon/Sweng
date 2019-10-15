27a1dadeabf6ba1fb47cf369f8d5b09c47dc50a4
christophernixon <chrisnixon31@gmail.com>
2019-10-15 18:05:50 +0100

    Added docstring with description of implementation, as well as outlines for required functions

--- lca.py
+++ lca.py
@@ -1,1 +1,10 @@
+"""Initial approach will be using simple algorithm: for finding LCA of n1 and n2,
+the path from root to n1 and root to n2 will be found and stored in two separate arrays.
+Both these arrays will be traversed similtaniously until a node with a differing key is found.
+This node will then be returned as the LCA."""
 import BTree
+
+def get_path(root, key, path):
+    return None
+def get_LCA(root, node1, node2):
+    return None
