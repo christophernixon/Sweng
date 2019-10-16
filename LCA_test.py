12b28e26bc61ede45c791fb42fb8557e328b1e01
christophernixon <chrisnixon31@gmail.com>
2019-10-16 13:32:03 +0100

    Modified syntax of LCA testing for increased readibility

--- LCA_test.py
+++ LCA_test.py
@@ -6,7 +6,7 @@ class test_lca(unittest.TestCase):
 
     def test_get_path(self):
         # First construct an arbitrary binary tree
-        tree = BTree.BTree(78, BTree.BTree(14), BTree.BTree(9))
+        tree = BTree.BTree(78, 14, 9)
         tree.insert_left_node(56)
         tree.insert_right_node(300)
         tree.insert_left_node(164)
@@ -14,7 +14,7 @@ class test_lca(unittest.TestCase):
         LCA.get_path(tree, 14, test_path)
         self.assertEqual(test_path, [78, 164, 56, 14], "Should be '[78, 164, 56, 14]'")
 
-        tree = BTree.BTree(1, BTree.BTree(2), BTree.BTree(3))
+        tree = BTree.BTree(1, 2, 3)
         tree.get_lt().insert_right_node(4)
         tree.get_rt().insert_left_node(5)
         test_path = []
@@ -26,7 +26,7 @@ class test_lca(unittest.TestCase):
 
     def test_get_lca(self):
         # First construct an arbitrary binary tree
-        tree = BTree.BTree(78, BTree.BTree(14), BTree.BTree(9))
+        tree = BTree.BTree(78, 14,9)
         tree.get_lt().insert_right_node(11)
         tree.get_lt().get_rt().insert_left_node(56)
         tree.get_lt().insert_left_node(300)
