4d7c4a905d8583c4148c0242b599f29f61c7e25e
christophernixon <chrisnixon31@gmail.com>
2019-11-28 18:58:44 +0000

    git_api.py: Cleaned up code, encapsulated functions within a class, made use of class variables and added extra information about the repository to the returned value. application.py: merged two api endpoints together to reduce code repeation, improved error checking. index.html: added basic information box to get details on selected repository.

--- git_api_project/application.py
+++ git_api_project/application.py
@@ -1,7 +1,8 @@
 import json
 import csv
+import logging
 import pandas as pd
-import git_api
+from git_api import git_api
 from flask import (Flask, abort, flash, jsonify, redirect, render_template,
                    request, send_file, send_from_directory, session)
 
@@ -23,53 +24,63 @@ data=DataStore()
 def homepage():
     return render_template("index.html")
 
-@application.route("/get-data",methods=["GET","POST"])
-def returnProdData():
-    # Get commits from repo
-    repo = 'sweng'
+@application.route("/weekday_data",methods=["GET","POST"])
+def handle_weekday_data_request():
+    # This is to allow for authenticated api requests
     user = 'christophernixon'
+
+    if request.method == 'POST':
+        repo = request.json.get('repo')
+    else:
+        # Since it is a GET request, the default repository data is supplied. 
+        repo = 'sweng'
     try:
-        commits = git_api.get_commits(repo,user)
-        return json.dumps(commits)
-    except Exception as e:
-        print(e)
-        
-@application.route("/search-repos",methods=["GET","POST"])
-def returnSearchData():
-    # Get commits from repo
-    repo = request.json.get('repo')
-    print(repo)
-    user = 'christophernixon'
-    try:
-        commits = git_api.get_commits(repo,user)
-        return json.dumps(commits)
-    except Exception as e:
-        print(e)
-    return
+        api = git_api(repo, user)
+        weekday_data = api.get_weekday_commit_freq()
+        return json.dumps(weekday_data)
+    except AttributeError as e:
+        logging.error(e)
+        abort(400)
 
 @application.route("/apache-data",methods=["GET","POST"])
 def returnApacheData():
-    with open('/Users/chrisnixon/yr3/sweng/git_api_project/apacheWeekdays.json', 'r') as f:
-        data = json.load(f)
-    return json.dumps(data)
+    try:
+        with open('/Users/chrisnixon/yr3/sweng/git_api_project/static/apacheWeekdays.json', 'r') as f:
+            data = json.load(f)
+        return json.dumps(data)
+    except Exception as e:
+        logging.error(e)
+        abort(400)
 
 @application.route("/nodejs-data",methods=["GET","POST"])
 def returnNodeJsData():
-    with open('/Users/chrisnixon/yr3/sweng/git_api_project/nodejsWeekdays.json', 'r') as f:
-        data = json.load(f)
-    return json.dumps(data)
+    try:
+        with open('/Users/chrisnixon/yr3/sweng/git_api_project/static/nodejsWeekdays.json', 'r') as f:
+            data = json.load(f)
+        return json.dumps(data)
+    except Exception as e:
+        logging.error(e)
+        abort(400)
 
 @application.route("/flutter-data",methods=["GET","POST"])
 def returnFlutterData():
-    with open('/Users/chrisnixon/yr3/sweng/git_api_project/flutterWeekdays.json', 'r') as f:
-        data = json.load(f)
-    return json.dumps(data)
+    try:
+        with open('/Users/chrisnixon/yr3/sweng/git_api_project/static/flutterWeekdays.json', 'r') as f:
+            data = json.load(f)
+        return json.dumps(data)
+    except Exception as e:
+        logging.error(e)
+        abort(400)
 
 @application.route("/typescript-data",methods=["GET","POST"])
 def returnTypescriptData():
-    with open('/Users/chrisnixon/yr3/sweng/git_api_project/typescriptWeekdays.json', 'r') as f:
-        data = json.load(f)
-    return json.dumps(data)
+    try:
+        with open('/Users/chrisnixon/yr3/sweng/git_api_project/static/typescriptWeekdays.json', 'r') as f:
+            data = json.load(f)
+        return json.dumps(data)
+    except Exception as e:
+        logging.error(e)
+        abort(400)
 
 if __name__ == "__main__":
     application.run(debug=True)
--- git_api_project/git_api.py
+++ git_api_project/git_api.py
@@ -6,55 +6,78 @@ from datetime import date
 import dateutil.parser
 import calendar
 
-def get_weekday_commit_freq(repo,user=''):
-    """Given a repository, this uses the github api to search for this repository,
-    and returns an array containing the counts of commits on each day for the first search result.
-    User parameter is optional, the github api allows more authenticated requests per hour."""
-    return format_raw_commit_data(get_commits(repo,user))
+class git_api(): 
+    response = None
+    repo = ""
+    user = ""
 
-def get_commits(repo,user=''):
-    """Searches for a particular repository using the search github API,
-    then returns all commits of the first repo."""
+    def __init__(self,repo,user=''):
+        self.repo = repo
+        self.user = user
+        self.response = None
 
-    # Attempting to use authenticated requests.
-    token = keyring.get_password('github', user)
-    if token == None:
-        github = Github()
-        logging.debug("No token set for user: %s",user)
-    else:
-        github = Github(token)
-    
-    top_search_results = github.search_repositories(query=repo)
-    top_repo = top_search_results[0]
-    return top_repo.get_commits()
-    
-
-def format_raw_commit_data(commits):
-    """Given a Paginated List of commits for a particular repository, this iterates through all the commits
-    and returns an array contain the count of commits for each day of the week."""
-
-    # Setting up a frequency dict
-    weekday_freq = {'Monday':0,
-                    'Tuesday':0,
-                    'Wednesday':0,
-                    'Thursday':0,
-                    'Friday':0,
-                    'Saturday':0,
-                    'Sunday':0}
-
-    for commit in commits:
-        date = commit._rawData['commit']['author']['date']
-        date_object = dateutil.parser.parse(date)
-        weekday = calendar.day_name[date_object.weekday()]
-        if weekday in weekday_freq:
-            weekday_freq[weekday] += 1
+    def get_weekday_commit_freq(self):
+        """Given a repository, this uses the github api to search for this repository,
+        and returns an array containing the counts of commits on each day for the first search result.
+        User parameter is optional, the github api allows more authenticated requests per hour."""
+        # Error check input
+        if self.repo == "":
+            raise AttributeError("Supplied repository name is empty.")
+        commits = self._get_commits()
+        weekday_count = self._format_raw_commit_data(commits)
+        if self.response is not None:
+            self.response['weekdays'] = weekday_count
         else:
-            weekday_freq[weekday] = 1
+            self.response = {'weekdays': weekday_count}
+        return self.response
 
-    # Formatting frequency dict into array
-    return_array = []
-    for weekday in weekday_freq:
-        tmp_dict = {'weekday':weekday, 'commits': weekday_freq[weekday]}
-        return_array.append(tmp_dict)
+    def _get_commits(self):
+        """Searches for a particular repository using the search github API,
+        then returns all commits of the first repo."""
 
-    return return_array
+        # Attempting to use authenticated requests.
+        token = keyring.get_password('github', self.user)
+        if token == None:
+            github = Github()
+            logging.debug("No token set for user: %s",self.user)
+        else:
+            github = Github(token)
+        
+        top_search_results = github.search_repositories(query=self.repo)
+        top_repo = top_search_results[0]
+        commits = top_repo.get_commits()
+
+        # Append some useful information about the repository to the response class variable.
+        self.response = {"description":top_repo.description, "full_name":top_repo.full_name,
+                    "language":top_repo.language,"stargazers_count":top_repo.stargazers_count}
+        return commits
+        
+    def _format_raw_commit_data(self,commits):
+        """Given a Paginated List of commits for a particular repository, this iterates through all the commits
+        and returns an array contain the count of commits for each day of the week."""
+
+        # Setting up a frequency dict
+        weekday_freq = {'Monday':0,
+                        'Tuesday':0,
+                        'Wednesday':0,
+                        'Thursday':0,
+                        'Friday':0,
+                        'Saturday':0,
+                        'Sunday':0}
+
+        for commit in commits:
+            date = commit._rawData['commit']['author']['date']
+            date_object = dateutil.parser.parse(date)
+            weekday = calendar.day_name[date_object.weekday()]
+            if weekday in weekday_freq:
+                weekday_freq[weekday] += 1
+            else:
+                weekday_freq[weekday] = 1
+
+        # Formatting frequency dict into array
+        return_array = []
+        for weekday in weekday_freq:
+            tmp_dict = {'weekday':weekday, 'commits': weekday_freq[weekday]}
+            return_array.append(tmp_dict)
+
+        return return_array
